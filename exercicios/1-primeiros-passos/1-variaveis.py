1a54dabf8ce0aea59b6efe2618874491b49b8382
kelvynmds <kelvynmoreira1@gmail.com>
2025-03-26 23:56:28 +0000

    kekel teste

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,19 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'kekel'
 
+obejtivo = 'nao endoidar'
+
+motivo = '''procurar
+um
+passatempo'''
 # Declare variáveis de cada tipo numérico
+idade = 30
 
+dinheiro = 3.070
 
+complexo = 2+1j
 # Declare uma variável do tipo booleano
+verdadeiro = True
+falso =  False
